7e3edf10fc25505c2ac658ad64a51ba9c8a57a9d
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-03-08 23:41:32 +0300

    Making Product model

---
+++ models/Product.js
@@ -0,0 +1,88 @@
+const { Schema, model } = require('mongoose')
+
+const ProductModel = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true,
+        maxLength: [100, 'Product name cannot exceed 100 characters']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please enter product price'],
+        maxLength: [5, 'Product price cannot exceed 5 characters'],
+        default: 0.0
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter product description']
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, 'Please select category for this Product'],
+        enum: {
+            values: [
+                'Electronics',
+                'Cameras',
+                'Laptop',
+                'Accessories',
+                'Headphones',
+                'Food',
+                'Books',
+                'Clothes/Shoes',
+                'Beauty/Health',
+                'Sports',
+                'Outdoor',
+                'Home'
+            ],
+            message: 'Please select correct category for this Product'
+        },
+        seller: {
+            type: String,
+            required: [true, 'Please enter product seller']
+        },
+        stock: {
+            type: Number,
+            required: [true, 'Please enter product stock'],
+            maxLength: 5
+        },
+        numOfReviews: {
+            type: Number,
+            default: 0
+        },
+        reviews: [
+            {
+                name: {
+                    type: String,
+                    required: true
+                },
+                rating: {
+                    type: Number,
+                    required: true
+                },
+                comment: {
+                    type: String,
+                    required: true
+                }
+            }
+        ]
+    }
+}, {timestamps: true})
+
+module.exports = model('Products', ProductModel)

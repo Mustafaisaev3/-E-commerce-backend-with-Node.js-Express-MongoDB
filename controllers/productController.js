61a70eab4a7c0bba153ab09d5c327278295baaa2
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-03-13 12:08:41 +0300

    Making filtering, sorting, pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,16 +6,50 @@ class ProductController {
         try {
             let queryObj = { ...req.query }
             const excludeFields = [ "page", "sort", "limit", "fields"]
-            excludeFields.forEach(el => delete queryObj[el])
 
+            // Filtering
+            excludeFields.forEach(el => delete queryObj[el])
             let queryStr = JSON.stringify(queryObj)
             queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
 
-            // console.log(queryObj, queryStr)
-            const products = await Product.find(JSON.parse(queryStr))
+
+            let query = Product.find(JSON.parse(queryStr))
+            
+            // Sorting
+            if(req.query.sort) {
+                const sortBy = req.query.sort.split(',').join(' ')
+                query = query.sort(sortBy)
+            } else {
+                query = query.sort('-createdAt')
+            }
+
+            // Limiting fields
+            if(req.query.fields) {
+                const fields = req.query.fields.split(',').join(' ')
+                query = query.select(fields)
+            } else {
+                query = query.select('-__v')
+            }
+
+            // Pagination
+            const page = req.query.page
+            const limit = req.query.limit
+            const skip = (page - 1) * limit
+            query = query.skip(skip).limit(limit)
+            if(req.query.page){
+                const productCount = await Product.countDocuments()
+                if(skip >= productCount) {
+                    console.log(productCount)
+                    // throw new Error('This Page does not exists')
+                    res.status(404).json({status: 'error', message: 'This Page does not exists'})
+                }
+            }
+
+            const products = await query
 
             res.status(200).json({status: 'success', products})
         } catch (error) {
+            console.log(error)
             res.status(400).json({error})
         }
     }
